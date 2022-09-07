f66bb9f822701b06d4940792199f0ddfbc5c3759
Ivan Catalano <96198463+OnizukaPy@users.noreply.github.com>
2022-09-07 13:38:47 +0200

    Add file via upload

---
+++ uppgifter.py
@@ -0,0 +1,16 @@
+value_int = int(10)
+value_string = "Python"
+value_float = 12.43
+
+while True:
+    value_in = int(input("Kunde du skriva ett tal? "))
+    if value_in > 0:
+        print("Talet är positiv")
+    elif value_in < 0:
+        print("Talet är negativ")
+    else:
+        print("Det är lika 0")
+    svar = input("Vill du fortsätta en gågn till s/n? ")
+    if svar != "s":
+        print("Hejdå")
+        break
