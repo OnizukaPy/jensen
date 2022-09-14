79fe44fd5497fbe4546b2721a17150186831dfd7
onizuka-py <onizuka.host@zohomail.eu>
2022-09-14 15:26:35 +0200

    ...

--- boken uppgifter/Kap 5/5.1.py
+++ boken uppgifter/Kap 5/5.1.py
@@ -1,4 +1,4 @@
 # svart kung https://unicode.org/charts/PDF/U2600.pdf
 # pi greek https://unicode.org/charts/PDF/U0370.pdf
 
-print("Jag är en svart kung i schack \u265A och en pi greek \u03C0")
+print("\nJag är en svart kung i schack \u265A och en pi greek \u03C0\n")
