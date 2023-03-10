3b1862768c2504b1479bb744338160e464521779
onizuka-py <onizuka.host@zohomail.eu>
2023-03-10 15:40:46 +0100

    nya uppgifter

---
+++ Utveckling av webbapplikationer/Javascript/21/21.1.js
@@ -0,0 +1,5 @@
+var friends = ["Lars", "Jan", "Peter", "Bo", "Frederik"];
+
+for( var i = 0; i < friends.length; i++ ) {  
+    alert( i ); // This loop will trigger three alerts, reading "0", "1", and "2" respectively.
+}
