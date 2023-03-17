477ad5268b690712e4cdea3dec26a15b9fdb5a77
onizuka-py <onizuka.host@zohomail.eu>
2023-03-17 09:26:54 +0100

    några uppdateringar

--- Utveckling av webbapplikationer/Flask/flask projekt/app.py
+++ Utveckling av webbapplikationer/Flask/flask projekt/app.py
@@ -1,35 +1,51 @@
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
-import os
-from PIL import Image
+# File: app.py
+# importering av moduler
 
+from flask import Flask, render_template, request     # flask modulen för att skapa webbapplikationer
+from werkzeug.utils import secure_filename            # werkzeug modulen för att hantera filer
+import os                                             # os modulen för att hantera filsystemet
+from PIL import Image                                 # PIL modulen för att hantera bilder och få bildens informationer
+
+# Konstanter
+# mappen där filerna ska sparas
 UPLOAD_FOLDER = '/home/onizuka-host/Zoho WorkDrive (Catalano Consulenze Tecniche)/My Folders/Documenti personali_/Corsi/Scuola di Python con Jensen/Esercizi/jensen/Utveckling av webbapplikationer/Flask/flask projekt/static/upload'
 
+# Flask applikationen
 app = Flask(__name__)
+
+# konfigurering av applikationen
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Flask routes
+# route för att visa formuläret
 @app.route('/')
 def upl():
-    return render_template('upload.html')
+   # returnera formuläret med hjälp av template filen upload.html
+   # som ligger i mappen templates
+   return render_template('upload.html')
 
+# route för att analisera uppladdningen av filen
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       name = secure_filename(f.filename)
-      complete_path = os.path.join(app.config['UPLOAD_FOLDER'], name)
+      complete_path = os.path.join(app.config['UPLOAD_FOLDER'], name)   # sökvägen till filen skapas med hjälp av os modulen
       f.save(complete_path)
       im = Image.open(complete_path)
 
+      # returnera resultatet av analysen med hjälp av template filen file.html 
+      # som ligger i mappen templates
       return render_template(
          'file.html', 
          name = name,
          filename = 'upload/'+name, 
-         filetype = f.headers['Content-Type'],
-         fileformat = im.size,
-         filesize = os.path.getsize(complete_path),
+         filetype = f.headers['Content-Type'],        # filtypen är i headers diktionäret med nyckeln 'Content-Type'
+         fileformat = im.size,                        # bildens storlek är i size attributet i Image objektet
+         filesize = os.path.getsize(complete_path),   # filens storlek får vi med hjälp av os modulen
          filepath = complete_path
          )
 
+# starta applikationen och sätt debug till True
 if __name__ == '__main__':
    app.run(debug = True)
--- Utveckling av webbapplikationer/Flask/flask projekt/readme.txt
+++ Utveckling av webbapplikationer/Flask/flask projekt/readme.txt
@@ -1,11 +1,9 @@
 FLASK MINIPROJEKT
 
-link github = 
-
-Gruppsmeddlen = Ivan, Bruno
+Gruppsmedlemar = Ivan, Bruno
 
 Istruktioner:
 
 1 - exekvera pip install -r requirements.txt
 2 - ladda ned zip-file från github
-3 - kör Python app.py och följa Istruktioner på skärmen 
+3 - extrakt filen och kör pythons coden app.py och följa istruktioner på skärmen 
